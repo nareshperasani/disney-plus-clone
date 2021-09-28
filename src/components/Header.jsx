5432a99fd144b72b37d488d2a71c9ce9786a8e89
nareshperasani <perasaninaresh@gmail.com>
2021-09-28 22:32:29 +0530

    ADD: image slider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,102 +1,103 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 function Header() {
-    return (
-        <Nav>
-            <Logo src="/images/logo.svg" />
-            <NavMenu>
-                <a>
-                    <img src="/images/home-icon.svg" alt="" />
-                    <span>HOME</span>
-                </a>
-                <a>
-                    <img src="/images/search-icon.svg" alt="" />
-                    <span>SEARCH</span>
-                </a>
-                <a>
-                    <img src="/images/watchlist-icon.svg" alt="" />
-                    <span>WATChLIST</span>
-                </a>
-                <a>
-                    <img src="/images/original-icon.svg" alt="" />
-                    <span>ORIGINAL</span>
-                </a>
-                <a>
-                    <img src="/images/movie-icon.svg" alt="" />
-                    <span>MOVIES</span>
-                </a>
-                <a>
-                    <img src="/images/series-icon.svg" alt="" />
-                    <span>SERIES</span>
-                </a>
-            </NavMenu>
-            <UserImg src="https://scontent.fvga3-1.fna.fbcdn.net/v/t1.6435-9/182425391_1798916623610512_4010424640486961607_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=-yFU7-XhMRUAX-GUw3p&_nc_ht=scontent.fvga3-1.fna&oh=890a9197f2cb1ae65ce9df1056118707&oe=6178DC95" />
-        </Nav>
-    )
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" alt="" />
+          <span>WATChLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" alt="" />
+          <span>ORIGINAL</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" alt="" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" alt="" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="https://scontent.fvga3-1.fna.fbcdn.net/v/t1.6435-9/182425391_1798916623610512_4010424640486961607_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=-yFU7-XhMRUAX-GUw3p&_nc_ht=scontent.fvga3-1.fna&oh=890a9197f2cb1ae65ce9df1056118707&oe=6178DC95" />
+    </Nav>
+  );
 }
 
 export default Header;
 
 const Nav = styled.div`
-    height: 70px;
-    background: #090b13;
-    display: flex;
-    align-items: center;
-    padding: 0 36px;
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+  overflow-x: hidden;
 `;
 
 const Logo = styled.img`
-    width: 80px;
-    cursor: pointer;
+  width: 80px;
+  cursor: pointer;
 `;
 const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+
+  a {
     display: flex;
-    flex: 1;
-    margin-left: 25px;
     align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
 
-    a{
-        display: flex;
-        align-items: center;
-        padding: 0 12px;
-        cursor: pointer;
-
-        img{
-            height: 20px;
-        }
-
-        span{
-            font-size: 13px;
-            letter-spacing: 1.4px;
-            position: relative;
-
-            &:after{
-                content: "";
-                height: 2px;
-                background: #fff;
-                position: absolute;
-                left: 0;
-                right: 0;
-                bottom: -6px;
-                opacity: 0;
-                transform-origin: left center;
-                transition: all 0.3s ease-in-out;
-                transform: scaleX(0);
-            }
-        }
-
-        &:hover{
-            span:after{
-                transform: scaleX(1);
-                opacity: 1;
-            }
-        }
+    img {
+      height: 20px;
     }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.4px;
+      position: relative;
+
+      &:after {
+        content: "";
+        height: 2px;
+        background: #fff;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transition: all 0.3s ease-in-out;
+        transform: scaleX(0);
+      }
+    }
+
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
+  }
 `;
 
 const UserImg = styled.img`
-    height: 50px;
-    border-radius: 50%;
-    cursor: pointer;
+  height: 50px;
+  border-radius: 50%;
+  cursor: pointer;
 `;
