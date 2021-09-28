0dabf0337c90d4b125cf40e0925a13b25d76d129
nareshperasani <perasaninaresh@gmail.com>
2021-09-28 23:00:56 +0530

    ADD: Viewers

--- src/components/Viewers.jsx
+++ src/components/Viewers.jsx
@@ -4,11 +4,51 @@ import styled from 'styled-components';
 function Viewers() {
     return (
         <Container>
-            Viewers
+            <Wrap>
+                <img src="/images/viewers-disney.png" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-marvel.png" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-pixar.png" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-national.png" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-starwars.png" alt="" />
+            </Wrap>
         </Container>
     )
 }
 
 export default Viewers;
 
-const Container = styled.div``;
+const Container = styled.div`
+    margin-top: 30px;
+    display: grid;
+    padding: 30px 0 26px;
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+    grid-gap: 25px;
+`;
+
+const Wrap = styled.div`
+    border: 3px solid rgba(249,249, 249, 0.1);
+    cursor: pointer;
+    border-radius: 10px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition: all 0.3s ease-in-out;
+    
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    &:hover{
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+        transform: scale(1.08);
+        border-color: rgba(249, 249, 249, 0.8);
+    }
+`;
